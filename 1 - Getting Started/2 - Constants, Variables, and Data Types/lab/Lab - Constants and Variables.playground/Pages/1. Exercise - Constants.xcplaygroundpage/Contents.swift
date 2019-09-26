c9d902b56880dde5b5b0533195d0c642443689c8
Scott Doggett <sdoggett@lcs.on.ca>
2019-09-26 16:13:48 -0400

    got to page 5

--- 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ 1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -9,10 +9,9 @@ print(friends)
  Now assume you go through and remove friends that aren't active on social media. Attempt to update your `friends` constant to a lower number than it currently is. Observe what happens and then move to the next step.
  */
 
-
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friends` constant to a lower number so that the playground will compile properly.
  */
-
+print("The change in value won't work because the let in the first statment makes friends a constant, which can't be changed unlike a variable which can be changed.")
 
 //: page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
